cc574e74bdd0bab92ae122f9adbeedea2601c5ac
alparslanKobak <alparslankobak@gmail.com>
2024-01-21 02:11:06 +0300

    Veritabanı işlemleri konfigürasyonu yapıldı

--- HotelArc.MVCUI/Program.cs
+++ HotelArc.MVCUI/Program.cs
@@ -1,9 +1,22 @@
+using HotelArc.Business;
+using HotelArc.Process.Abstract;
+using HotelArc.Process.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddTransient(typeof(IService<>), typeof(Service<>));
 
 var app = builder.Build();
 
@@ -26,5 +39,25 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+using (var scope = app.Services.CreateScope())
+{
+
+    var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+
+    var db = dbContext.Database;
+
+    if (await db.CanConnectAsync())
+    {
+        await db.EnsureDeletedAsync(); 
+    }
+
+    if (!await db.CanConnectAsync())
+    {
+        await db.EnsureCreatedAsync(); 
+
+        DbSeeder.Seed(dbContext);
+    }
+}
 
 app.Run();
