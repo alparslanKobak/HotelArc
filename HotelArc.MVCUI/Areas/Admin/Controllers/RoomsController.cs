a9f0d12c9f648aef98e4665015e5d4170329cc9a
alparslanKobak <alparslankobak@gmail.com>
2024-01-22 19:23:10 +0300

    .

---
+++ HotelArc.MVCUI/Areas/Admin/Controllers/RoomsController.cs
@@ -0,0 +1,95 @@
+﻿using HotelArc.Kernel.Entities;
+using HotelArc.Process.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelArc.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoomsController : Controller
+    {
+        private readonly IRoomService _roomService;
+
+        public RoomsController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        // GET: RoomsController
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Room> rooms = await _roomService.GetRoomsByIncludeAsync();
+
+            return View(rooms);
+        }
+
+        // GET: RoomsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RoomsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoomsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoomsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RoomsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoomsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RoomsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
