19a89ec7040ba824c020856c5788d82f68b6913c
alparslanKobak <alparslankobak@gmail.com>
2024-01-26 00:58:21 +0300

    .

---
+++ HotelArc.MVCUI/Areas/Admin/Controllers/ReservationController.cs
@@ -0,0 +1,94 @@
+﻿using HotelArc.Kernel.Entities;
+using HotelArc.Process.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelArc.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReservationController : Controller
+    {
+        private readonly IReservationService _reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        // GET: ReservationController
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Reservation> reservations = await  _reservationService.GetReservationsByIncludeAsync();
+            return View(reservations);
+        }
+
+        // GET: ReservationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReservationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReservationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
